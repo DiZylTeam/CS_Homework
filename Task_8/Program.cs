41b6e07145acab2c17b3c909a9c4e1fbdda7d9d3
Dmitry <9554300@gmail.com>
2023-05-31 18:41:03 +0300

    Added new folder task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+int minNumber = 1; // give a value of min number
+if (number <= 1)
+{
+    Console.WriteLine("Invalid value!");
+}
+    while (minNumber <= number)
+    {
+        if (minNumber % 2 == 0)
+        {
+            Console.Write(minNumber + " ");
+        }
+    {
+        minNumber = minNumber + 1;
+    }
+    }
