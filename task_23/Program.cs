ab91a8d39f014fe82816685344b28d6e419ba0c1
Dmitry <9554300@gmail.com>
2023-06-08 21:43:49 +0300

    Added task23

---
+++ task_23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+
+int Prompt(string massage) 
+{
+    Console.Write(massage);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void FindCube(int num_n)
+{
+for (int n = 1; n <= num_n; n++)
+{
+int result = n * n * n;
+Console.Write(result + " ");
+}
+}
+
+int resNum = Prompt("Enter a number: ");
+FindCube(resNum);
