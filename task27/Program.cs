c34ea5ed224be9146ff09da0124f1556e03dd2b9
unknown <9554300@gmail.com>
2023-06-13 16:10:10 +0300

    added task27

---
+++ task27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+int Prompt(string message)
+{
+Console.Write(message);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+int SumOfAllDigits(int num)
+{
+int sum = 0;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+return sum;
+}
+
+int number = Prompt("Enter a number: ");
+Console.WriteLine($"Sum of all number's digits: {SumOfAllDigits(number)}"); // сумма всех чисел числа
+
+
