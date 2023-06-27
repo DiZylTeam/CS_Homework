192ebbd830a43e242e37b8b3b9b40c6ba7326a3d
Dmitry <9554300@gmail.com>
2023-06-27 15:54:48 +0300

    Added task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+
+void GetNumbers(int number)
+{
+    if (number >= 1)
+    {
+        Console.Write($"{number} ");
+        GetNumbers(number - 1);  // рекурсия, т.е. использование этого же метода в этом же методе
+    }
+}
+
+
+int number = Prompt("Enter a natural number: ");
+if (number <= 0) Console.WriteLine($"Invalid value");
+GetNumbers(number);
+
+
+
+
+
+
